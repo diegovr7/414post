f999bb12c461e68631bb06a2d893ea51f5796268
diegovr7 <diegovr7@gmail.com>
2019-05-10 12:51:15 -0500

    Add files

---
+++ assets/js/data.js
@@ -0,0 +1,18 @@
+angular.module('myApp', []).controller('proyectosCtrl', function($scope) {
+    $scope.proyectos = [
+        
+        
+        {proyecto:'Lala',descripcion:'El perrito que se cayó',link:"https://www.youtube.com/watch?v=aYsFb7S8LFc",img:"assets/img/portfolio/port-1.jpg", alt:"lala publicidad", categoria: "1, 3"},
+        {proyecto:'Jani',descripcion:'Norway',link:"https://www.youtube.com/watch?v=aYsFb7S8LFc",img:"assets/img/portfolio/port-1.jpg", alt:"lala publicidad", categoria: "2"},
+        {proyecto:'Lala',descripcion:'El perrito que se cayó',link:"https://www.youtube.com/watch?v=aYsFb7S8LFc",img:"assets/img/portfolio/port-1.jpg", alt:"lala publicidad", categoria: "1, 4"},
+        {proyecto:'Lala',descripcion:'El perrito que se cayó',link:"https://www.youtube.com/watch?v=aYsFb7S8LFc",img:"assets/img/portfolio/port-1.jpg", alt:"lala publicidad", categoria: "1, 2, 3"},
+        {proyecto:'Lala',descripcion:'El perrito que se cayó',link:"https://www.youtube.com/watch?v=aYsFb7S8LFc",img:"assets/img/portfolio/port-1.jpg", alt:"lala publicidad", categoria: "3, 4"},
+        {proyecto:'Lala',descripcion:'El perrito que se cayó',link:"https://www.youtube.com/watch?v=aYsFb7S8LFc",img:"assets/img/portfolio/port-1.jpg", alt:"lala publicidad", categoria: "1, 3"},
+        {proyecto:'Lala',descripcion:'El perrito que se cayó',link:"https://www.youtube.com/watch?v=aYsFb7S8LFc",img:"assets/img/portfolio/port-1.jpg", alt:"lala publicidad", categoria: "4"},
+        {proyecto:'Lala',descripcion:'El perrito que se cayó',link:"https://www.youtube.com/watch?v=aYsFb7S8LFc",img:"assets/img/portfolio/port-1.jpg", alt:"lala publicidad", categoria: "1, 3"},
+        {proyecto:'Lala',descripcion:'El perrito que se cayó',link:"https://www.youtube.com/watch?v=aYsFb7S8LFc",img:"assets/img/portfolio/port-1.jpg", alt:"lala publicidad", categoria: "1, 2"},
+        {proyecto:'Lala',descripcion:'El perrito que se cayó',link:"https://www.youtube.com/watch?v=aYsFb7S8LFc",img:"assets/img/portfolio/port-1.jpg", alt:"lala publicidad", categoria: "1, 3"},
+        {proyecto:'Lala',descripcion:'El perrito que se cayó',link:"https://www.youtube.com/watch?v=aYsFb7S8LFc",img:"assets/img/portfolio/port-1.jpg", alt:"lala publicidad", categoria: "1, 3"},
+      
+    ];
+}); 
